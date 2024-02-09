789a052c8f68616c97cfca5c0b9be150a52e0776
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-02-09 12:07:17 +0530

    added : socket added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,27 @@
 const express = require('express');
 const { createServer } = require('node:http');
-
+const cors= require("cors")
 const app = express();
+const {Server} = require("socket.io")
 const server = createServer(app);
+app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
-});
+const io = new Server(server,{
+  cors:{
+    origin:"http://localhost:300",
+    methods:["GET","POST"],
+  }
+})
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+io.on("connection",(socket:any)=>{
+  console.log(socket.id)
+
+  socket.on("disconnect",()=>{
+    console.log("user disconnected",socket.id)
+  })
+})
+
+
+server.listen(3001, () => {
+  console.log('server running at http://localhost:3001');
 });
