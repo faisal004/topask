9b8464ac6fb0e073c5791e53a58ad7d1d6bc4243
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-02-16 15:59:20 +0530

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,7 +24,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin:process.env.FRONTENDPORT,
+    origin:"*",
     methods: ['GET', 'POST'],
   },
 });
